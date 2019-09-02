b4747e11adfa248319ed8e13823034eeebeff837
JuanJo <sr.naxter@gmail.com>
2019-09-02 12:28:42 +0200

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,10 @@
+from Juego import *
+from Aplicacion import *
+from Manzana import *
+from Jugador import *
+
+# Recuerda tener todos los archivos en una misma carpeta
+# para que los imports no tengan problemas.
+if __name__ == '__main__':
+    aplicacion = Aplicacion()
+    aplicacion.on_execute()
